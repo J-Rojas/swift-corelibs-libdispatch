9062425d069f863e2fb76b1a99471cf3e6be9a2a
Jose Rojas <jrojas@redlinesolutions.co>
2018-02-13 19:51:50 -0800

    Simplify build-android.sh with minimal configuration.

--- build-android.sh
+++ build-android.sh
@@ -30,18 +30,7 @@ ANDROID_STANDALONE_SYSROOT=$ANDROID_STANDALONE_TOOLCHAIN/sysroot
 export PATH=$ANDROID_STANDALONE_TOOLCHAIN/bin:$PATH
 export CC=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-clang
 export CXX=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-clang++
-export AR=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-ar
-export AS=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-as
-export LD=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-ld
-export RANLIB=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-ranlib
-export NM=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-nm
-export STRIP=$ANDROID_STANDALONE_TOOLCHAIN/bin/arm-linux-androideabi-strip
-export CHOST=arm-linux-androideabi
-export ARCH_FLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=vfpv3-d16"
 export ARCH_LINK="-march=armv7-a -Wl,--fix-cortex-a8 -L$ANDROID_ICU_PATH/armeabi-v7a/ -L$ANDROID_STANDALONE_TOOLCHAIN/lib/gcc/arm-linux-androideabi/4.9.x/ -lgcc"
-export CPPFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing "
-export CXXFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing -frtti -fexceptions -std=c++11 -Wno-error=unused-command-line-argument "
-export CFLAGS=" ${ARCH_FLAGS} -fpic -ffunction-sections -funwind-tables -fstack-protector -fno-strict-aliasing "
 export LDFLAGS=" ${ARCH_LINK} "
 
 if [ ! -f libdispatch.so ]; then
@@ -55,14 +44,13 @@ if [ ! -f libdispatch.so ]; then
                     -DCMAKE_C_COMPILER="$CC" \
                     -DCMAKE_C_COMPILER_TARGET="armv7-none-linux-androideabi" \
                     -DCMAKE_CXX_COMPILER="$CXX" \
-                    -DCMAKE_C_FLAGS="-Wno-error=unknown-attributes -Wno-error=unused-function -DDEPLOYMENT_TARGET_ANDROID -DDEPLOYMENT_ENABLE_LIBDISPATCH --sysroot=$ANDROID_STANDALONE_SYSROOT -I$ANDROID_STANDALONE_SYSROOT/usr/include -I$ANDROID_ICU_PATH/armeabi-v7a/include -I${SDKROOT}/lib/swift" \
+                    -DCMAKE_C_FLAGS="--sysroot=$ANDROID_STANDALONE_SYSROOT -I$ANDROID_STANDALONE_SYSROOT/usr/include -I$ANDROID_ICU_PATH/armeabi-v7a/include -I${SDKROOT}/lib/swift" \
                     -DCMAKE_SWIFT_FLAGS="-I$ANDROID_STANDALONE_SYSROOT/usr/include" \
                     -DCMAKE_SYSTEM_NAME=Android \
                     -DCMAKE_SYSTEM_PROCESSOR=armv7l \
                     -DENABLE_SWIFT=1 \
                     -DENABLE_TESTING=1 \
                     -DDISPATCH_ENABLE_ASSERTS=0 \
-                    -DCMAKE_REQUIRED_INCLUDES="$ANDROID_ICU_PATH/armeabi-v7a/include ${SDKROOT}/lib/swift $ANDROID_STANDALONE_SYSROOT/usr/include" \
                     $SWIFT_PATH/swift-corelibs-libdispatch
 
                 cp -r /usr/include/uuid $ANDROID_STANDALONE_SYSROOT/usr/include 
